1514a4446b64cb680a8517980b51203f27abc80a
Christian Deras <alexderas12@outlook.com>
2023-04-23 23:14:52 -0600

    Agregando formatos

--- resources/views/remodelacion/index.blade.php
+++ resources/views/remodelacion/index.blade.php
@@ -3,16 +3,17 @@
 @section('contenidoPrincipal')
 <div class="container">
     <div class="col-12 mb-3 text-center mt-5">
-      <h2>Por favor rellene el siguiente formulario</h2>
-  </div>
+        <h2>Por favor rellene el siguiente formulario</h2>
+    </div>
     <div class="row mt-5 d-flex align-items-center justify-content-center position-relative ">
         <div class="circulo">
 
         </div>
         <div class="col-md-6 col-sm-12">
-            <form class="row g-3 needs-validation p-3 border rounded-2 bg-white" action="/remodelaciones" method="POST" novalidate>
-            @csrf    
-            <div class="col-12">
+            <form class="row g-3 needs-validation p-3 border rounded-2 bg-white" action="/remodelaciones" method="POST"
+                novalidate>
+                @csrf
+                <div class="col-12">
                     <label for="Nombre" class="form-label">Nombre completo:</label>
                     <input type="text" id="nombre" name="nombrecompleto" class="form-control" required>
                     <div class="valid-feedback">
@@ -22,7 +23,7 @@
                         Por favor ingrese su nombre completo
                     </div>
                 </div>
-                                
+
                 <div class="col-8">
                     <label for="correo" class="form-label">Correo:</label>
                     <input type="mail" id="correo" name="correo" class="form-control" required>
@@ -46,11 +47,12 @@
 
                 <div class="col-12">
                     <div class="form-check form-check-inline">
-                        <input class="form-check-input" type="radio"  id="inline1" value="Remodelacion" name="tipo" required>
+                        <input class="form-check-input" type="radio" id="inline1" value="Remodelacion" name="tipo"
+                            required>
                         <label class="form-check-label" for="inline1">Remodelacion</label>
                     </div>
                     <div class="form-check form-check-inline">
-                        <input class="form-check-input" type="radio"  id="inline2" name="tipo" value="nueva" required>
+                        <input class="form-check-input" type="radio" id="inline2" name="tipo" value="nueva" required>
                         <label class="form-check-label" for="inline2">Construccion nueva</label>
                     </div>
                     <div class="valid-feedback">
@@ -63,14 +65,15 @@
                 <div class="col-4">
                     <label for="money" class="form-label">Presupuesto:</label>
                     <div class="input-group">
-                    <span class="input-group-text">$</span>
-                    <input type="text" class="form-control" id="presupuesto" name="presupuesto" aria-label="Amount (to the nearest dollar)" required>
-                    <div class="valid-feedback">
-                        Todo bien
-                    </div>
-                    <div class="invalid-feedback">
-                        Por favor pon un rango valido.
-                    </div>
+                        <span class="input-group-text">$</span>
+                        <input type="text" class="form-control" id="presupuesto" name="presupuesto"
+                            aria-label="Amount (to the nearest dollar)" required>
+                        <div class="valid-feedback">
+                            Todo bien
+                        </div>
+                        <div class="invalid-feedback">
+                            Por favor pon un rango valido.
+                        </div>
                     </div>
                 </div>
                 <div class="col-4">
@@ -93,32 +96,32 @@
                         Por favor ingrese su municipio.
                     </div>
                 </div>
-                
-                
-                    <button type="submit" class="btn btn-primary mt-3 mb-3 mt-2 btn-color">Enviar</button>
+
+
+                <button type="submit" class="btn btn-primary mt-3 mb-3 mt-2 btn-color">Enviar</button>
             </form>
         </div>
-        
+
     </div>
-  </div>
-  <script>
-    (() => {
-  'use strict'
+</div>
+<script>
+(() => {
+    'use strict'
 
-  // Fetch all the forms we want to apply custom Bootstrap validation styles to
-  const forms = document.querySelectorAll('.needs-validation')
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    const forms = document.querySelectorAll('.needs-validation')
 
-  // Loop over them and prevent submission
-  Array.from(forms).forEach(form => {
-    form.addEventListener('submit', event => {
-      if (!form.checkValidity()) {
-        event.preventDefault()
-        event.stopPropagation()
-      }
+    // Loop over them and prevent submission
+    Array.from(forms).forEach(form => {
+        form.addEventListener('submit', event => {
+            if (!form.checkValidity()) {
+                event.preventDefault()
+                event.stopPropagation()
+            }
 
-      form.classList.add('was-validated')
-    }, false)
-  })
+            form.classList.add('was-validated')
+        }, false)
+    })
 })()
-  </script>
+</script>
 @endsection
--- resources/views/vivienda/viviendas.blade.php
+++ resources/views/vivienda/viviendas.blade.php
@@ -2,144 +2,150 @@
 
 @section('contenidoPrincipal')
 <div class="container-fluid">
-        <div class="row mt-5">
-            <div class="col-md-6 col-sm-12 ">
-                <!-- Slider main container -->
-                <div class="swiper">
-                    <!-- Additional required wrapper -->
-                    <div class="swiper-wrapper">
-                        <!-- Slides -->
-                        <div class="swiper-slide">
-                            <div class="container  d-flex justify-content-center align-items-center">
-                                <img src="/Img/rascacielos.jpg" alt="" class="img-fluid rounded-4" width="400">
+    <div class="row mt-5">
+        <div class="col-md-6 col-sm-12 ">
+            <!-- Slider main container -->
+            <div class="swiper">
+                <!-- Additional required wrapper -->
+                <div class="swiper-wrapper">
+                    <!-- Slides -->
+                    <div class="swiper-slide">
+                        <div class="container  d-flex justify-content-center align-items-center">
+                            <img src="/Img/rascacielos.jpg" alt="" class="img-fluid rounded-4" width="400">
 
-                            </div>
                         </div>
-                        <div class="swiper-slide">
-                            <div class="container  d-flex justify-content-center align-items-center">
-                                <img src="/Img/rascacielos.jpg" alt="" class="img-fluid rounded-4" width="400">
-
-                            </div>
-                        </div>
-                        <div class="swiper-slide">
-                            <div class="container  d-flex justify-content-center align-items-center">
-                                <img src="/Img/rascacielos.jpg" alt="" class="img-fluid rounded-4" width="400">
-
-                            </div>
-                        </div>
-                        
                     </div>
+                    <div class="swiper-slide">
+                        <div class="container  d-flex justify-content-center align-items-center">
+                            <img src="/Img/rascacielos.jpg" alt="" class="img-fluid rounded-4" width="400">
 
+                        </div>
+                    </div>
+                    <div class="swiper-slide">
+                        <div class="container  d-flex justify-content-center align-items-center">
+                            <img src="/Img/rascacielos.jpg" alt="" class="img-fluid rounded-4" width="400">
 
-                    <!-- If we need navigation buttons -->
-                    <div class="swiper-button-prev"></div>
-                    <div class="swiper-button-next"></div>
+                        </div>
+                    </div>
 
                 </div>
-                <h3>Servicios:</h3>
-                    <div class="col-md-12 col-sm-12 col-xxl-12">
-                        <div class=" data d-flex justify-content-center align-items-center ">
-                            <ul class="font-rowdies-scalable d-flex justify-content-center flex-wrap ul-info align-items-center">
-                                <li class="w-100">Vista al mar</li>
-                                <li class="w-100">Centros comerciales cercanos</li>
-                                <li class="w-100">1 cochera</li>
-                            </ul>
-                        </div>
-                    </div>
 
-                    <h3>!Hey veamos una muestra en 360°¡</h3>
-                    <div class="col-md-12 col-sm-12 col-xxl-12">
-                        <button type="submit" class="btn btn-color" data-bs-toggle="modal" data-bs-target="#exampleModal">Abrir 360°</button>
-                    </div>
+
+                <!-- If we need navigation buttons -->
+                <div class="swiper-button-prev"></div>
+                <div class="swiper-button-next"></div>
+
             </div>
-            <div class="col-md-6 col-sm-12">
-                <h2 class="font-rowdies text-center">{{$vivienda->nombreinmueble}}</span></h2>
-                <h3>Detalles:</h3>
-                <div class="row g-3">
-                    <div class="col-md-6 col-sm-12 col-xxl-12  ">
-                        <div class="data d-flex justify-content-around align-items-center ">
-                            <div class="circle-sm d-flex justify-content-center align-items-center">
-                                <i class="fa-solid fa-house "></i>
-                            </div>
-                            <span class="font-rowdies-scalable">Habitaciones:  <span><b>{{$vivienda->habitaciones}}</b></span></span>
+            <h3>Servicios:</h3>
+            <div class="col-md-12 col-sm-12 col-xxl-12">
+                <div class=" data d-flex justify-content-center align-items-center ">
+                    <ul
+                        class="font-rowdies-scalable d-flex justify-content-center flex-wrap ul-info align-items-center">
+                        <li class="w-100">Vista al mar</li>
+                        <li class="w-100">Centros comerciales cercanos</li>
+                        <li class="w-100">1 cochera</li>
+                    </ul>
+                </div>
+            </div>
+
+            <h3>!Hey veamos una muestra en 360°¡</h3>
+            <div class="col-md-12 col-sm-12 col-xxl-12">
+                <button type="submit" class="btn btn-color" data-bs-toggle="modal" data-bs-target="#exampleModal">Abrir
+                    360°</button>
+            </div>
+        </div>
+        <div class="col-md-6 col-sm-12">
+            <h2 class="font-rowdies text-center">{{$vivienda->nombreinmueble}}</span></h2>
+            <h3>Detalles:</h3>
+            <div class="row g-3">
+                <div class="col-md-6 col-sm-12 col-xxl-12  ">
+                    <div class="data d-flex justify-content-around align-items-center ">
+                        <div class="circle-sm d-flex justify-content-center align-items-center">
+                            <i class="fa-solid fa-house "></i>
                         </div>
+                        <span class="font-rowdies-scalable">Habitaciones:
+                            <span><b>{{$vivienda->habitaciones}}</b></span></span>
                     </div>
-                    <div class="col-md-6 col-sm-12 col-xxl-12">
-                        <div class="data d-flex justify-content-around align-items-center ">
-                            <div class="circle-sm d-flex justify-content-center align-items-center">
-                                <i class="fa-solid fa-toilet"></i>
-                            </div>
-                            <span class="font-rowdies-scalable">Cantidad de baños: <span><b>{{$vivienda->sanitarios}}</b></span></span>
+                </div>
+                <div class="col-md-6 col-sm-12 col-xxl-12">
+                    <div class="data d-flex justify-content-around align-items-center ">
+                        <div class="circle-sm d-flex justify-content-center align-items-center">
+                            <i class="fa-solid fa-toilet"></i>
                         </div>
+                        <span class="font-rowdies-scalable">Cantidad de baños:
+                            <span><b>{{$vivienda->sanitarios}}</b></span></span>
                     </div>
-                    <div class="col-md-12 col-sm-12 col-xxl-12  ">
-                        <div class="data d-flex justify-content-center align-items-center ">
-                            <div class="circle-sm d-flex justify-content-center align-items-center">
-                                <i class="fa-solid fa-house "></i>
-                            </div>
-                            <span class="font-rowdies-scalable">Cantidad de plantas: <span><b>{{$vivienda->plantas}}</b></span></span>
+                </div>
+                <div class="col-md-12 col-sm-12 col-xxl-12  ">
+                    <div class="data d-flex justify-content-center align-items-center ">
+                        <div class="circle-sm d-flex justify-content-center align-items-center">
+                            <i class="fa-solid fa-house "></i>
                         </div>
+                        <span class="font-rowdies-scalable">Cantidad de plantas:
+                            <span><b>{{$vivienda->plantas}}</b></span></span>
                     </div>
-                    <h3>Descripcion:</h3>
-                    <div class="col-md-12 col-sm-12 col-xxl-12">
-                        <div class=" data d-flex justify-content-around align-items-center ">
-                            <span>
+                </div>
+                <h3>Descripcion:</h3>
+                <div class="col-md-12 col-sm-12 col-xxl-12">
+                    <div class=" data d-flex justify-content-around align-items-center ">
+                        <span>
                             {{$vivienda->detalle}}
                             <br>
                             <br>
                             {{$vivienda->descripcion}}
-                            </span>
-                        </div>
+                        </span>
                     </div>
                 </div>
-                
             </div>
+
         </div>
     </div>
-    <div class="modal fade" id="exampleModal" tabindex="-1" style="z-index: 9999;" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-xl">
-          <div class="modal-content">
+</div>
+<div class="modal fade" id="exampleModal" tabindex="-1" style="z-index: 9999;" aria-labelledby="exampleModalLabel"
+    aria-hidden="true">
+    <div class="modal-dialog modal-xl">
+        <div class="modal-content">
             <div class="modal-header">
-              <h1 class="modal-title fs-5" id="exampleModalLabel">Visualizer</h1>
-              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                <h1 class="modal-title fs-5" id="exampleModalLabel">Visualizer</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
                 <a-scene>
                     <a-sky src="/Img/Buhardilla 360 Inmobiliaria - Descargas360.jpg" rotation="0 -130 0"></a-sky>
-              
+
                     <a-text font="kelsonsans" value="Puy de Sancy, France" width="6" position="-2.5 0.25 -1.5"
-                            rotation="0 15 0"></a-text>
-                  </a-scene>
+                        rotation="0 15 0"></a-text>
+                </a-scene>
             </div>
             <div class="modal-footer">
-              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-              <button type="button" class="btn btn-primary">Save changes</button>
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Save changes</button>
             </div>
-          </div>
         </div>
-      </div>
-    <script src="/Js/main.js"></script>
-    <script>
-        const swiper = new Swiper('.swiper', {
-            // Optional parameters
-            direction: 'horizontal',
-            loop: true,
+    </div>
+</div>
+<script src="/Js/main.js"></script>
+<script>
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
 
-            // If we need pagination
-            pagination: {
-                el: '.swiper-pagination',
-            },
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
 
-            // Navigation arrows
-            navigation: {
-                nextEl: '.swiper-button-next',
-                prevEl: '.swiper-button-prev',
-            },
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
 
-            // And if we need scrollbar
-            scrollbar: {
-                el: '.swiper-scrollbar',
-            },
-        });
-    </script>
+    // And if we need scrollbar
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+});
+</script>
 @endsection
